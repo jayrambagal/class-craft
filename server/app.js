85d9b6cef924d86b6a5a8e4916034c46eecc6194
Jayram Bagal <94613732+jayrambagal@users.noreply.github.com>
2023-03-12 23:38:40 +0530

    Successfully adding authentication with JWT tokens

--- server/app.js
+++ server/app.js
@@ -1,20 +1,23 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
 
 const app = express()
+app.use(cookieParser())
 app.use(express.json())
+
 app.use(require("./router/auth"))
-const path ="mongodb+srv://Jayram:jayram1234@cluster0.dmbhtsw.mongodb.net/ClassCarft?retryWrites=true&w=majority"
 
-app.get("/",(req,res)=>{
-    res.send("hellp")
+port  = process.env.PORT || 5002
 
-})
+const path = process.env.DB_PATH
+
 
 mongoose.connect(path)
   .then(() => console.log("Database connected!"))
   .catch(err => console.log(err));
 
-app.listen(5000,async ()=>{
-    console.log("Connected to server :)")
+app.listen(port,async ()=>{
+  console.log(`Your port is ${port}`)
 });
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,8 +1,13 @@
 const express = require("express")
+const bcrypt = require("bcryptjs")
 const router = express.Router()
-
+const jwt = require("jsonwebtoken")
+require('dotenv').config()
+require("../app");
 const User = require("../DataBase/userSchema")
+const Authenticate = require("../Middleware/Authentication")
 require("cookie-parser")
+
 
 router.post("/register",async(req,res)=>{
     const {email,password} = req.body
@@ -20,7 +25,7 @@ router.post("/register",async(req,res)=>{
         else{
             const user = new User({email,password})
             await user.save()
-            res.status(201).json({message:"sucsessfull"})
+            res.status(200).json({message:"sucsessfull"})
         }   
 
     }catch(err){
@@ -34,24 +39,59 @@ router.post("/login",async(req,res)=>{
         const {email,password} = req.body
 
         if(!email || !password){
-            res.status(400).json({error:"email or password is require"})
+            res.status(400).json({error:"email and password booth are require"})
         }
 
         const userLogin = await User.findOne({email:email})
 
         if(!userLogin){
-            res.status(400).json({error:"user not found"})
+            res.status(400).json({error:"invalid password"})   
         }
         else{
-            if(password== userLogin.password){
-                res.status(200).json({message:"user login succesfully"})
+            
+            const isMatch = await bcrypt.compare(password,userLogin.password);
+
+            // adding jwt token
+            const token = jwt.sign(
+                { user_id: User._id, email },
+                process.env.private_key,
+                {
+                  expiresIn: "2h",
+                }
+              );
+              // save user token
+              User.token = token;
+        
+            // store jwt token in cookie 
+            res.cookie("jwttoken",token,{
+                expires:new Date(Date.now()+25892000000),
+                httpOnly:true
+            })
+
+            if(!isMatch){
+                console.log("we are in not ismatch")
+                res.status(400).json({message:"invalid password"})
             }
             else{
-                res.status(400).json({message:"invalid password"})
+                res.status(200).json({message:"user login succesfully"})
             }
         }
 
-    }catch(err){console.log(err)}
+    }catch(err){
+        console.log("error in signin auth")
+        console.log(err)
+    }
+})
+
+router.get("/home",Authenticate,(req,res)=>{
+    res.status(200).send("Welcome 🙌 ");
+})
+
+router.get("/logout",(req,res)=>{
+    console.log("we are in logout")
+    res.clearCookie("jwttoken",{path:"/"})
+    res.status(200).send("user logout")
 })
 
 module.exports = router
+
